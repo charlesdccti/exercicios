1ec05935f715d81da3c923d7609b813f984b0062
charlesdccti <charlesdccti@gmail.com>
2013-08-21 20:51:22 -0300

    Olá mundo!

---
+++ src/com/charles/exemploGit/OlaMundo.java
@@ -0,0 +1,13 @@
+package com.charles.exemploGit;
+
+public class OlaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá Mundo!");
+	}
+
+}
